4e3cbe7f56d0ee3efba248a645cd06d988859a97
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-04-17 11:27:48 +0530

    Create inquiry.js and product.js files in models folder

---
+++ backend/models/inquiry.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const inquirySchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  response: {
+    type: String,
+    required: true,
+     default: "No response yet"
+  },
+  isResolved: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+});
+
+const Inquiry = mongoose.model("Inquiry", inquirySchema);
+export default Inquiry;
---
+++ backend/models/product.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+
+const productSchema = new mongoose.Schema({
+    key :{
+       type : String,
+       required : true,
+       unique : true
+    },
+    name :{
+       type : String,
+       required : true
+    },
+    price :{
+        type : String,
+        required : true
+    },
+    category :{
+      type : String,
+      required : null,
+      default : "uncategorized"
+    },
+    dimentions : {
+       type : String,
+       required : true
+    },
+    description :{
+        type : String,
+        required : true
+    },
+    availability :
+    {
+       type : Boolean,
+       required : true,
+       default :true
+    },
+    image:{
+      type : [String],
+      required : true,
+      default :["https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fuser%2F&psig=AOvVaw0uCU9t72Sm51_RBTIJ2OxU&ust=1739456529253000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMCbiuCqvosDFQAAAAAdAAAAABAE"]
+    }
+})
+
+
+const Product = mongoose.model("product",productSchema);
+
+export default Product;
