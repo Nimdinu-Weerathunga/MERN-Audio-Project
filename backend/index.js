0cc42841cacdf49bbb0e89d20b34923849ba36a3
Weerathunga T K T N <135198957+Nimdinu-Weerathunga@users.noreply.github.com>
2025-04-17 20:38:30 +0530

    Completed

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,76 @@
-import express from "express"
 
-let app = express ()
+//import the express dependency for using our project.
+
+import express from "express"
+//for using 
+import bodyParser from "body-parser";
+//import the mongoose library for using our software
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv";
+import reviewRouter from "./routes/reviewRouter.js";
+import InquiryRouter from "./routes/inquiryRouter.js";
+import cors from "cors";
+
+
+
+//create a vearible for express coll
+const app = express();
+app.use(cors());
+
+dotenv.config();
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+   let token = req.header("Authorization");
+
+   if (token) {
+       token = token.replace("Bearer ", "");
+
+       jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+           if (err) {
+               console.error("JWT Verification Failed:", err.message);
+           } else {
+               req.user = decoded;
+           }
+       });
+   }
+
+   next();
+});
+
+
+
+
+//create a database connection 
+/**
+ * if i using the connection database string ,i can connect with data
+ * base accesse inside  the code
+ */
+let mongoUrl = process.env.MONGO_URL;
+
+mongoose.connect(mongoUrl)
+
+let connection = mongoose.connection
+
+connection.once("open",()=>{
+   console.log("Mongo db connection successfully")
+})
+//** middleware
+// reading the token and identify the user and 
+// include user details for httprequest */
+//start middleware 
+
+
+
+//routing 
+app.use("/api/users",userRouter)
+app.use("/api/product",productRouter)
+app.use("/api/reviews",reviewRouter)
+app.use("/api/inquiry",InquiryRouter)
 
 app.listen(3000,()=>{
-    console.log("Server is running om port 3000")
+   console.log("Server is runnin on port 3000"); 
 })
